87845dc9718a0f930f927e6132eb439f5c57fdff
Maxim Lyutikov <hackmailmaxim@gmail.com>
2023-06-27 02:33:17 +0300

    Commit 9.0 (version 26.06.2023)
    + bugfixes with the custom player "hello" from the server

--- PALATA_RaidPlugin/src/main/java/org/palata_raidplugin/palata_raidplugin/PlayerJoinListener.java
+++ PALATA_RaidPlugin/src/main/java/org/palata_raidplugin/palata_raidplugin/PlayerJoinListener.java
@@ -7,23 +7,48 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PlayerJoinListener implements Listener {
     private final PALATA_RaidPlugin plugin;
 
+    private Map<String, String> playerNames = new HashMap<>();
+
     public PlayerJoinListener(PALATA_RaidPlugin plugin) {
         this.plugin = plugin;
+
+        // Получаем список строк из конфигурации
+        List<String> playersList = plugin.getConfig().getStringList("players");
+
+        // Обрабатываем каждую строку
+        for (String player : playersList) {
+            // Разделяем строку по символу ':'
+            String[] parts = player.split(":");
+
+            // Если строка корректно разделена на две части, сохраняем их в словарь
+            if (parts.length == 2) {
+                playerNames.put(parts[0], parts[1]);
+            }
+        }
+
+        // Теперь словарь playerNames содержит имена игроков и их реальные имена
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        FileConfiguration config = plugin.getConfig(); // Получаем конфигурацию плагина
-        String realName = config.getString("players." + player.getName()); // Получаем реальное имя игрока из конфигурации
-        if (realName != null) { // Если имя не null, используем его для сообщения
+
+        // Получаем реальное имя из словаря
+        String realName = playerNames.get(player.getName());
+
+        if (realName != null) {
             player.sendMessage(ChatColor.LIGHT_PURPLE + "Здарова, " + realName + ". Рад тебя видеть.");
-        } else { // Если имя null, используем имя игрока для сообщения
+        } else {
             player.sendMessage(ChatColor.LIGHT_PURPLE + "Здарова, " + player.getName() + ". Рад тебя видеть.");
         }
+
         plugin.getGame().updateScoreboard();
         player.setScoreboard(plugin.getGame().getScoreboard());
     }
